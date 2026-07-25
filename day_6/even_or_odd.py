e825dde378f7e6ee0076f131aa1e3d2f3ca0586a
eswar-sai-2 <eswars106@gmail.com>
2026-07-25 06:18:52 +0530

    day_6 - even_or_odd

---
+++ day_6/even_or_odd.py
@@ -0,0 +1,13 @@
+num = int(input("enter a number : "))
+
+if num % 2 == 0 :
+    print("The number", num, "is Even.")
+else :
+    print("The number", num, "is Odd.")
+
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print(f"The number {num} is Even.")
+# else:
+#     print(f"The number {num} is Odd.")
