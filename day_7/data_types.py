fa200d00d0f1199988a604ac14a601df2e27b45d
eswar-sai-2 <eswars106@gmail.com>
2026-07-25 06:28:33 +0530

    day_7 - data_types

---
+++ day_7/data_types.py
@@ -0,0 +1,16 @@
+# data types 
+
+name = "Sai eswar"
+age  = 19
+cgpa = 8.15
+is_student = True
+
+print("Name :", name)
+print("age :", age)
+print("CGPA :", cgpa)
+print("STUDENT :", is_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
