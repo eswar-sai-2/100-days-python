76be30e4900f8c4543fe4487e1cbb83c15b15bdf
eswar-sai-2 <eswars106@gmail.com>
2026-07-25 06:09:32 +0530

    day_5 - basic_calculator

---
+++ day_5/basic_calculator.py
@@ -0,0 +1,11 @@
+n1 = int(input("enter first value:"))
+n2 = int(input("enter second value:"))
+
+print("All answers like addition, subtraction, multiplication, division, floor division, power")
+
+print("Addition : ", n1 + n2)
+print("Subtraction : ", n1 - n2)
+print("Multiplication : ", n1 * n2)
+print("Division : ", n1 / n2)
+print("Floor Division :", n1 // n2)
+print("Power : ", n1 ** n2)
