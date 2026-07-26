3a97ff146216531dcadcae28302a95e7f1a2f406
eswar-sai-2 <eswars106@gmail.com>
2026-07-26 22:20:58 +0530

    day_9 - swapping

---
+++ day_9/swapping.py
@@ -0,0 +1,11 @@
+a = int(input("Enter one value: "))
+b = int(input("Enter second value: "))
+
+print("Before Swapping : ")
+print("a value is : ", a)
+print("b value is : ", b)
+
+a , b = b , a 
+print("After Swapping : ")
+print("a value is : ", a)
+print("b value is : ", b)
