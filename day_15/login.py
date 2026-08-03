bf0a31ac4ceb775d3900742ec16ba670702d7797
eswar-sai-2 <eswars106@gmail.com>
2026-08-03 20:55:10 +0530

    day_15 - login_system

---
+++ day_15/login.py
@@ -0,0 +1,29 @@
+# username = "errorythm"
+# password = "12345"
+
+# u = input("Enter Username: ")
+# p = input("Enter Password: ")
+
+# if u == username and p == password:
+#     print("Login Successfully")
+# else:
+#     print("Login Failed!")
+
+username = "sai-eswar"
+password = "220"
+
+for i in range(3):
+    u = input("Enter Username : ")
+    p = input("Enter Password : ")
+    if u == username and p == password:
+        print("Login Successfully")
+        break
+    else:
+        print(f"Attempt {i + 1} failed.")
+        print("Login Failed!")
+else :
+    print("Your 3 attempts are finished")
+
+
+
+    
